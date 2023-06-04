5ae3e19fdff85f9f0bd2e578adc8734d77159d80
Pavel M <p.mordowsky@gmail.com>
2023-06-04 08:03:49 +0200

    Rallback changes

--- pdp/src/App.tsx
+++ pdp/src/App.tsx
@@ -2,21 +2,15 @@ import React, { Suspense, useState } from "react";
 
 import "./index.scss";
 
-const Header = React.lazy(() => import("home/Header"));
+import Header from "home/Header";
 import Footer from "home/Footer";
 
-const App = () => {
-  const [showHeader, setShowHeader] = useState(false);
-
-  return (<div className="mt-10 text-3xl mx-auto max-w-6xl">
-    { showHeader && <Suspense fallback={<div>Loading...</div>}>
-        <Header />
-      </Suspense>
-    }
-    <button className="text-3xl p-5" onClick={() => setShowHeader(!showHeader)}>Toggle Header</button>
+const App = () =>
+(<div className="mt-10 text-3xl mx-auto max-w-6xl">
+    <Header />
     <div className="my-10">PDP page content</div>
     <Footer />
   </div>
-)};
+);
 
 export default App;
