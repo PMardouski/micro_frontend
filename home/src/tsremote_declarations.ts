43da25086614c1ec1292200bd30cf081e59fc689
Pavel M <p.mordowsky@gmail.com>
2023-07-15 06:48:38 +0200

    fix marckup issue

--- home/src/tsremote_declarations.ts
+++ home/src/tsremote_declarations.ts
@@ -1,4 +1,6 @@
 declare module "home/Header";
 declare module "home/Footer";
 declare module "home/products";
+declare module "cart/MiniCart";
+declare module "cart/Login";
 declare module "cart/cart";
